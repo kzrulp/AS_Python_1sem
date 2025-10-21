a9f993b4f6fa4b89baf62d5210fdc85af8159532
kzrulp <kuptsova.ulya@bk.ru>
2025-10-21 12:57:45 +0300

    Update main.py

--- 1.1lab/main.py
+++ 1.1lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+# https://math-ege.sdamgia.ru/problem?id=27954 - задлание номер 9 из егэ по профильной математике
+r = 500
+v = 300
+f = 700000
+prof = 300000
+q = ((prof + f) / (r - v))
+print(int(q))
