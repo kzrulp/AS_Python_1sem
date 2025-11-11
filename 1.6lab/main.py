54ec9b03f5179dd64f4bd3bff93104470bd7e510
kzrulp <kuptsova.ulya@bk.ru>
2025-11-11 14:47:05 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+#Дана строка-предложение. Подсчитайте вхождение каждого слова в данное предложение.
+predl = input("Введите предложение: ").lower().split()
+
+word_ct = {}
+for word in predl:
+    word_ct[word] = word_ct.get(word, 0) + 1
+
+print("Результат:")
+for word, count in word_ct.items():
+    print(f"'{word}': {count}")
+
