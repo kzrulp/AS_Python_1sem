315fdf5ad5929a1317329fe0e22685ff31147492
kzrulp <kuptsova.ulya@bk.ru>
2025-11-11 14:41:18 +0300

    Update main.py

--- 1.5lab/main.py
+++ 1.5lab/main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    pass
+# Дан список. Выбрать из списка все числа меньше 15 и упорядочить их по убыванию.
+num = list(map(int, input("Введите список: ").split()))
+men15 = []
+for x in num:
+    if x < 15:
+        men15.append(x)
+men15.sort(reverse=True)
+
+print(men15)
