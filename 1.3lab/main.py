808e1188d43273d113cf65fbb626c8bedb6efb16
kzrulp <kuptsova.ulya@bk.ru>
2025-11-11 14:22:26 +0300

    Update main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    pass
+#Для заданного натурального n и действительного x подсчитать сумму:\begin{align} \sin{x} + \sin{x^2} + \sin{x^3} + ... + \sin{x^n}. \end{align}
+
+код:
+import math
+
+n = int(input("n: "))
+x = float(input("x: "))
+
+t = sum(math.sin(x ** k) for k in range(1, n + 1))
+print(f"Сумма = {t}")
