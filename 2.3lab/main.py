b9d0f08e4b27f8b84b12fc5a997b845c6579a2c3
kzrulp <kuptsova.ulya@bk.ru>
2025-12-02 12:16:06 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,25 @@
-if __name__ == "__main__":
-    pass
+def InvertStr(S, K, N):
+    if K > len(S):
+        return ""
+
+    start = K - 1
+
+    end = start + N
+
+    if end > len(S):
+        end = len(S)
+
+    substring = S[start:end]
+
+    return substring[::-1]
+
+
+S = "программирование"
+
+print(InvertStr(S, 3, 5))
+
+print(InvertStr(S, 20, 5))
+
+print(InvertStr(S, 12, 10))
+
+print(InvertStr(S, 1, 20))
