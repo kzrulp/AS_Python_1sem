089f1ef75fd800bd9d608a706170fc168276f4be
kzrulp <kuptsova.ulya@bk.ru>
2025-12-14 16:00:19 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,18 @@
-if __name__ == "__main__":
-    pass
+f = open('f.txt', 'r')
+
+num = f.readlines()
+f.close()
+
+max_abs = 0
+i = 1   
+
+for l in num:
+    x = float(l)
+
+    if i % 2 == 1:   
+        if abs(x) > max_abs:
+            max_abs = abs(x)
+
+    i = i + 1
+
+print(max_abs)
