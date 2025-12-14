f192849973bb5d54f9cb50b3120bc4f2aa33111a
kzrulp <kuptsova.ulya@bk.ru>
2025-12-14 15:53:04 +0300

    Update main.py

--- 2.6lab/main.py
+++ 2.6lab/main.py
@@ -1,2 +1,29 @@
-if __name__ == "__main__":
-    pass
+def remove_digits(filename):
+    f = open(filename, 'r')
+    text = f.read()
+    f.close()
+
+    new_text = ''
+    for c in text:
+        if c < '0' or c > '9':
+            new_text = new_text + c
+
+    f = open(filename, 'w')
+    f.write(new_text)
+    f.close()
+
+
+def count_symbols(filename):
+    f = open(filename, 'r')
+    text = f.read()
+    f.close()
+
+    return len(text)
+
+
+def count_lines(filename):
+    f = open(filename, 'r')
+    lines = f.readlines()
+    f.close()
+
+    return len(lines)
