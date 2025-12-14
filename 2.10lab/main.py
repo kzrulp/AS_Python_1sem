f05c8414b2a1e94d96e90517ae1ae11f5a56c27b
kzrulp <kuptsova.ulya@bk.ru>
2025-12-14 16:35:59 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,31 @@
-if __name__ == "__main__":
-    pass
+а)
+n = int(input())
+
+if n < 0:
+    raise ValueError("Отрицательное число")
+
+f = 1
+for i in range(1, n + 1):
+    f *= i
+
+print(f)
+
+б)
+n = int(input())
+
+if n <= 0:
+    raise ValueError("Число не является положительным")
+
+print("Число является положительным")
+
+
+в)
+n = int(input())
+
+assert n >= 0, "Отрицательное число"
+
+f = 1
+for i in range(1, n + 1):
+    f *= i
+
+print(f)
