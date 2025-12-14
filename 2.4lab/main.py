4252e741906c44e788434461d791f396e6569d00
kzrulp <kuptsova.ulya@bk.ru>
2025-12-14 15:34:49 +0300

    Update main.py

--- 2.4lab/main.py
+++ 2.4lab/main.py
@@ -1,2 +1,51 @@
-if __name__ == "__main__":
-    pass
+а)
+def capitalize_words(text, separator=' '):
+    parts = text.split(separator)
+    result = []
+
+    for word in parts:
+        result.append(word.capitalize())
+
+    return separator.join(result)
+
+б)
+def filter_elements(list1, list2, filter_function=None):
+    result = []
+
+    for x in list1:
+        if filter_function is None or filter_function(x):
+            result.append(x)
+
+    for x in list2:
+        if filter_function is None or filter_function(x):
+            result.append(x)
+
+    return result
+
+в)
+def merge_dictionaries(*dicts):
+    result = {}
+
+    for d in dicts:
+        for key in d:
+            result[key] = d[key]
+
+    return result
+
+
+г)
+def unique_keys(*dicts):
+    result = {}
+
+    for i in range(len(dicts)):
+        for key in dicts[i]:
+            count = 0
+
+            for j in range(len(dicts)):
+                if key in dicts[j]:
+                    count += 1
+
+            if count == 1:
+                result[key] = dicts[i][key]
+
+    return result
