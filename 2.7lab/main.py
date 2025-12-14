556958e617b0bf14ce6452464bd7f9b2c51a6660
kzrulp <kuptsova.ulya@bk.ru>
2025-12-14 15:57:42 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__":
-    pass
+f = open('text.txt', 'r')
+lines = f.readlines()
+f.close()
+
+f = open('text.txt', 'w')
+
+for l in lines:
+    l = line[:-1]   
+
+    if l != '':
+        if len(l) % 2 == 1:
+            l = ' ' + l
+
+        k = (50 - len(l)) // 2
+        f.write(' ' * k + l)
+
+    f.write('\n')
+
+f.close()
