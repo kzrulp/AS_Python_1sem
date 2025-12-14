ab4c58beff3a49c35f61592dcd8a71d615040060
kzrulp <kuptsova.ulya@bk.ru>
2025-12-14 15:39:54 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,17 @@
-if __name__ == "__main__":
-    pass
+def Simm(S):
+    if len(S) <= 1:
+        return True
+
+    if S[0] != S[-1]:
+        return False
+
+    return Simm(S[1:-1])
+
+
+проверка:
+text = input("Введите строку: ")
+
+if Simm(text):
+    print("Строка симметрична")
+else:
+    print("Строка не симметрична")
