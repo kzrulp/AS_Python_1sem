ef79b576f40a0718be16fac52a1890ac45b02a28
kzrulp <kuptsova.ulya@bk.ru>
2025-12-14 16:03:18 +0300

    Update main.py

--- 2.9lab/main.py
+++ 2.9lab/main.py
@@ -1,2 +1,33 @@
-if __name__ == "__main__":
-    pass
+f = open('input.txt', 'r')
+
+k = int(f.readline())
+
+f1 = open('sym.txt', 'w')
+f2 = open('other.txt', 'w')
+
+for t in range(k):
+    n = int(f.readline())
+    a = []
+
+    for i in range(n):
+        a.append(f.readline().split())
+
+    ok = True
+
+    for i in range(n):
+        for j in range(n):
+            if a[i][j] != a[j][i]:
+                ok = False
+
+    if ok:
+        f1.write(str(n) + '\n')
+        for i in range(n):
+            f1.write(' '.join(a[i]) + '\n')
+    else:
+        f2.write(str(n) + '\n')
+        for i in range(n):
+            f2.write(' '.join(a[i]) + '\n')
+
+f.close()
+f1.close()
+f2.close()
